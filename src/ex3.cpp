7f2f82998a10a34ef142ecd4f3e18d45347f191d
Robin De Moor <robindemoor04@gmail.com>
2025-04-22 10:21:32 +0300

    updated ex3

--- src/ex3.cpp
+++ src/ex3.cpp
@@ -5,31 +5,32 @@ class Car{
     private:
         string brand;
         int year;
+        
     public:
-        void Constructor(){     //lets user create a car of choice
-            string b;
-            int y;
-            cout<<"\nBrand: ";
-            cin>>b;
-            cout<<"\nYear: ";
-            cin>>y;
-            brand=b;
-            year=y;
-            cout<<"Car "<<b<<" from "<<y<<" created.";
+        string* br=&brand;
+        int* yr=&year;
+        Car(string b,int y){
+            *br=b;
+            *yr=y;
+            cout<<"\nCar "<<*br<<" from "<<*yr<<" created.";
         }
-        void Destructor(){
-            year=0;
-            cout<<"\nCar "<<brand<<" destroyed.";
-            brand="NULL";
+        ~Car(){
+            cout<<"Car "<<*br<<" destroyed.";
+            delete br;
+            delete yr;
         }
         void showInfo(){
-            cout<<"\nBrand: "<<brand<<"\nYear: "<<year;
+            cout<<"\nBrand: "<<*br<<"\nYear: "<<*yr;
+        }
+        void test(){
+            cout<<brand<<year;
         }
 };
 
 int main(){
-    Car car1;
-    car1.Constructor();
-    car1.showInfo();
-    car1.Destructor();
+    Car* c1=new Car("bobin",1984);
+    c1->showInfo();
+
+    delete c1;
+    test();
 }    
